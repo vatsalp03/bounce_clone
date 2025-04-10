e7ffe9ae070599a663d84391abf7d7e6823fdc81
Vatsal Parmar <vatsalparmar123123@gmail.com>
2025-04-10 19:57:23 +0530

    added killzone and enemies to level 1

--- Scenes/player_ball.gd
+++ Scenes/player_ball.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
-@onready var sprite_2d = $Sprite2D
 
+@onready var sprite_2d = $Sprite2D
 
 @export var speed = 150
 @export var jumpforce = 365
 
+const gravity = 1000
+const player_weight = 1
+var jump_counter = 0
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		velocity.y += gravity * player_weight * delta
+		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jumpforce
 
@@ -17,11 +20,11 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		
-	if direction > 0:
+		velocity.x = 0
+
+	if direction == 1:
 		sprite_2d.flip_h = false
-	elif direction < 0:
+	elif direction == -1:
 		sprite_2d.flip_h = true
 
 	move_and_slide()
